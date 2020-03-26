771410e31ca1969ff2eda61f08fbe6282c162076
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-03-26 16:07:11 -0400

    network scanner change 1

--- network_scanner/netparse.py
+++ network_scanner/netparse.py
@@ -6,12 +6,12 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1,verbose = False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1,verbose = False)[0] # srp - send recieve packets ,used[0] because srp usually returns ans and unans
     
     print("IP\t\t\tMAC Address\n------------------------------------------")
     
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        print(element[1].psrc + "\t\t" + element[1].hwsrc) #psrc ip of client hwsrc mac of destination
         
 
-scan("10.0.2.2/24")
+scan("10.0.3.15/24")
