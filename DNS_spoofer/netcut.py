662d34330901e58773a9c63c7e1242e3e70d4b5d
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-09 13:52:14 -0400

    before dns

--- DNS_spoofer/netcut.py
+++ DNS_spoofer/netcut.py
@@ -6,14 +6,25 @@ import subprocess
 
 def create_queue():
     subprocess.call(["sudo","iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", "0"])
+    print("[+] Creating queue")
+
+def delete_queue():
+    subprocess.call(["sudo","iptables","--flush"])
+    print("[-] Deleting queue")
+
 
 def process_packet(packet):
     print(packet)
     #packet.accept()
 
 create_queue()
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0,process_packet)
-queue.run()
+try:
+    while(True):
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0,process_packet)
+        queue.run()
+except(KeyboardInterrupt):
+    print("\n")
+    delete_queue()
 
 
